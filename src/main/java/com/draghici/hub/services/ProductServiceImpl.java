6ecd4c3943945c61eda19c272ffe47548e045cea
IonutD <draghiciionutcatalin@gmail.com>
2024-08-02 22:05:53 +0300

    [ID] added method for getAll in ProductServiceImpl

--- src/main/java/com/draghici/hub/services/ProductServiceImpl.java
+++ src/main/java/com/draghici/hub/services/ProductServiceImpl.java
@@ -2,15 +2,26 @@ package com.draghici.hub.services;
 
 import com.draghici.hub.beans.Product;
 import com.draghici.hub.dto.ProductDTO;
+import com.draghici.hub.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final static Logger logger = LogManager.getLogger(ProductServiceImpl.class);
+
+    private final ProductRepository productRepository;
+
     @Override
     public Page<Product> getAll(Pageable pageable) {
-        return null;
+        logger.info("Request: getAll");
+        return productRepository.findAll(pageable);
     }
 
     @Override
