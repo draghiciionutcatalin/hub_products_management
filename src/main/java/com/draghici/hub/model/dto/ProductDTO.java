c18f10d59fb46adf2c4369f15dc0fe9f348bac4c
IonutD <draghiciionutcatalin@gmail.com>
2024-08-02 20:49:37 +0300

    [ID] updated ProductDTO with annotations from Lombok

--- src/main/java/com/draghici/hub/model/dto/ProductDTO.java
+++ src/main/java/com/draghici/hub/model/dto/ProductDTO.java
@@ -1,8 +1,14 @@
 package com.draghici.hub.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
     String name;
     Double price;
