27847fcd71d99156d91162b62d2aeb63fe3fcfda
IonutD <draghiciionutcatalin@gmail.com>
2024-08-03 11:47:06 +0300

    [ID] implemented ProductException in the ProductServiceImpl

--- src/main/java/com/draghici/hub/services/ProductServiceImpl.java
+++ src/main/java/com/draghici/hub/services/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.draghici.hub.services;
 
 import com.draghici.hub.beans.Product;
 import com.draghici.hub.dto.ProductDTO;
+import com.draghici.hub.exceptions.ProductException;
 import com.draghici.hub.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -17,6 +18,8 @@ import java.util.Optional;
 public class ProductServiceImpl implements ProductService {
 
     private final static Logger logger = LogManager.getLogger(ProductServiceImpl.class);
+    private final static int HTTP_NOT_FOUND = 404;
+    private final static int HTTP_UNPROCESSABLE_ENTITY = 422;
 
     private final ProductRepository productRepository;
 
@@ -31,7 +34,7 @@ public class ProductServiceImpl implements ProductService {
         logger.info("API Request: get one product by id");
 
         if (id < 0) {
-            throw new Exception("A product with negative id cannot exist");
+            productNegativeIdException();
         }
         return productRepository.getProductById(id).orElseThrow(() -> productMissingException(id));
     }
@@ -41,19 +44,19 @@ public class ProductServiceImpl implements ProductService {
         logger.info("API Request: add new product");
 
         if (productDto == null) {
-            throw new Exception("Cannot add a null product");
+            throw new ProductException("Cannot add a null product", HTTP_UNPROCESSABLE_ENTITY);
+        }
+        if ("".equals(productDto.getName()) || productDto.getPrice() < 0.0d) {
+            throw new ProductException("Please provide a name and a price for the product", HTTP_UNPROCESSABLE_ENTITY);
         }
 
-        if (!"".equals(productDto.getName()) && productDto.getPrice() > 0.0d) {
-            var product = new Product();
-            product.setName(productDto.getName());
-            product.setPrice(productDto.getPrice());
-            var newProduct = productRepository.save(product);
-            logger.info("A product was added with id {}", newProduct.getId());
-            return newProduct;
-        } else {
-            throw new Exception("Please provide a name and a price to the product");
-        }
+        var product = new Product();
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        var newProduct = productRepository.save(product);
+
+        logger.info("A product was added with id {}", newProduct.getId());
+        return newProduct;
     }
 
     @Override
@@ -61,7 +64,7 @@ public class ProductServiceImpl implements ProductService {
         logger.info("API Request: update product");
 
         if (productDto == null) {
-            throw new Exception("Cannot update a null product");
+            throw new ProductException("Cannot update a null product", HTTP_UNPROCESSABLE_ENTITY);
         }
 
         var product = productRepository.getProductById(id).orElseThrow(() -> productMissingException(id));
@@ -80,7 +83,7 @@ public class ProductServiceImpl implements ProductService {
         logger.info("API Request: delete product");
 
         if (id < 0) {
-            throw new Exception("A product with negative id cannot exist");
+            productNegativeIdException();
         }
 
         var product = productRepository.getProductById(id).orElseThrow(() -> productMissingException(id));
@@ -90,6 +93,10 @@ public class ProductServiceImpl implements ProductService {
     }
 
     private static Exception productMissingException(Long id) {
-        return new Exception("Product with id " + id + " not found");
+        return new ProductException("Product with id " + id + " not found", HTTP_NOT_FOUND);
+    }
+
+    private static Exception productNegativeIdException() {
+        return new ProductException("A product with negative id cannot exist", HTTP_UNPROCESSABLE_ENTITY);
     }
 }
